1be0dacb111551f43ebdedcf82e23d6639606923
fedek1324 <fedua1324@yandex.ru>
2022-10-30 15:01:02 +0300

    fixed all

--- App/components/KeyboardSpacer.js
+++ App/components/KeyboardSpacer.js
@@ -1,45 +1,48 @@
-import React, { useState, useEffect } from "react"
-import { Keyboard, View, Dimensions, Platform, StyleSheet } from "react-native"
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Keyboard, Dimensions, Platform } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
+    top: 0,
     left: 0,
-    right: 0,
     bottom: 0,
   },
-})
+});
 
 export const KeyboardSpacer = ({ style, onToggle = () => null }) => {
-  const [keyboardSpace, setKeyboardSpace] = useState(0)
+  const [keyboardSpace, setKeyboardSpace] = useState(0);
 
   useEffect(() => {
+    // useEffect выполняется при рендеринге страницы и выполняет функцию в return после выхода с экрана
+
     const updateKeyboardSpace = event => {
       if (!event.endCoordinates) {
-        return
+        return;
       }
-
-      const screenHeight = Dimensions.get("window").height
-      const newKeyboardSpace = screenHeight - event.endCoordinates.screenY
-      setKeyboardSpace(newKeyboardSpace)
-      onToggle(true, newKeyboardSpace)
-    }
+      const screenHeight = Dimensions.get("screen").height;
+      const newKeyboardSpace = screenHeight - event.endCoordinates.screenY;
+      setKeyboardSpace(newKeyboardSpace);
+      onToggle(true, newKeyboardSpace);
+      console.log(newKeyboardSpace)
+    };
     const showEvt =
-      Platform.OS === "android" ? "keyboardDidShow" : "keyboardWillShow"
-    const showListener = Keyboard.addListener(showEvt, updateKeyboardSpace)
+      Platform.OS === "android" ? "keyboardDidShow" : "keyboardWillShow";
+    const showListener = Keyboard.addListener(showEvt, updateKeyboardSpace);
 
     const resetKeyboardSpace = () => {
-      setKeyboardSpace(0)
-      onToggle(false, 0)
-    }
+      console.log(0)
+      setKeyboardSpace(0);
+      onToggle(false, 0);
+    };
     const hideEvt =
       Platform.OS === "android" ? "keyboardDidHide" : "keyboardWillHide"
-    const hideListener = Keyboard.addListener(hideEvt, resetKeyboardSpace)
+    const hideListener = Keyboard.addListener(hideEvt, resetKeyboardSpace);
 
     return () => {
-      showListener.remove()
-      hideListener.remove()
-    }
-  }, [])
+      showListener.remove();
+      hideListener.remove();
+    };
+  }, []); // [] - это зависимости, задают когда активироваться помимо рендера
 
-  return <View style={[styles.container, { height: keyboardSpace }, style]} />
-}
+  return <View style={[styles.container, { height: keyboardSpace }, style]} />;
+};
